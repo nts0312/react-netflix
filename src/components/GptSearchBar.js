56f7da5dd25f8fac87a58c534c5a038ecfca57d2
nts0312 <nitinsinghm111@gmail.com>
2024-02-23 09:45:13 +0530

    fixed errors
    fixed errors

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import { lang } from "../utils/languageConstants";
+import openai from "../utils/openai";
 
 const GptSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
 
-  const searchText = useRef(null)
-  const handleGptSearchClick = (e) => {
+  const searchText = useRef(null);
+
+  const handleGptSearchClick = async (e) => {
     e.preventDefault();
-    console.log(e.target.value)
+    //from here call to gpt api
+
+    const gptResults = await openai.chat.completions.create({
+      messages: [{ role: "user", content: e.target.value }],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(gptResults.choices)
   };
 
   return (
     <div className="pt-[12%] flex justify-center">
       <form className="bg-black w-1/2 grid grid-cols-12 rounded-lg">
         <input
-        ref={searchText}
+          ref={searchText}
           type="text"
           className="p-4 m-4 col-span-9 rounded-lg"
           placeholder={lang[langKey].gptSearchPlaceholder}
